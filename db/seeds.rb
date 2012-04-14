db7fb5ad53ef5733019c3619fddcb29a693f2d12
Ryan Resella <ryan@codeforamerica.org>
2012-04-13 17:54:11 -0700

    Remove data from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-unless Rails.env.production?
-  Precinct.create([{number: 1, description: 'VALENCIA LIBRARY'},{number: 2, description: 'VALENCIA UNITED METHODIST CHURCH'}])
-  Candidate.create([{ name: 'Rick Smith'}, { name: 'Bob Jones'}])
-end
