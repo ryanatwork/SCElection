c10a8dfaaec4fe5e1212c35d9fc2677fa0a9ebd4
Ryan Resella <ryan@codeforamerica.org>
2012-04-21 12:27:35 -0700

    Remove gmaps for now

--- app/models/precinct.rb
+++ app/models/precinct.rb
@@ -2,13 +2,10 @@ class Precinct < ActiveRecord::Base
   attr_accessible :description, :number, :location, :latitude, :longitude
   has_many :votes
 
-  acts_as_gmappable
-
   def gmaps4rails_address
     #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.location}, Santa Clarita, CA USA"
   end
-
 
   def precinct_full
     self.number.to_s + '-' + self.description
