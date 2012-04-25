414104735ed186b1a84de8e22479f8f2dc26c9c3
Ryan Resella <ryan@codeforamerica.org>
2012-04-24 18:45:35 -0700

    Wrote specs for precint

--- spec/models/precinct_spec.rb
+++ spec/models/precinct_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Precinct do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    FactoryGirl.create(:precinct)
+  end
+
+  describe "#gmaps4rails_address" do
+    it "should return the concat name" do
+      test = Precinct.last
+      test.gmaps4rails_address.should == "12345 Main Street, Santa Clarita, CA USA"
+    end
+  end
+
+  describe "#precinct_full" do
+    it "should return full precinct info" do
+      test = Precinct.last
+      test.precinct_full.should == "1-Santa Clarita Library"
+    end
+  end
+
+  describe "#full_address" do
+    it "should return the full address" do
+      test = Precinct.last
+      test.full_address.should == "12345 Main Street Santa Clarita, CA"
+    end
+  end
+
 end
